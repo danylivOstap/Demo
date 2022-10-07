1be2cd59bdd06ca8faed00bfd1950a7480249aa4
danylivOstap <danyliv.ostap@gmail.com>
2022-10-07 20:04:40 +0300

    Add existing project files to Git

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/test")
+    public String showInfo() {
+        return "App is working"
+               + "\n App is working"
+               + "\n App is working"
+               + "\n App is working"
+               + "\n App is working"
+               + "\n App is working"
+               + "\n App is working"
+               + "\n App is working"
+               + "\n App is working"
+               + "\n App is working";
+    }
+}
